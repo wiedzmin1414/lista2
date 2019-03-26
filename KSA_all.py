f72492cab5279d53d610cac8417dd22f0c31c153
witcher1414 <lukaszpolakiewicz14@gmail.com>
2019-03-26 21:54:01 +0100

    xD

--- KSA_all.py
+++ KSA_all.py
@@ -18,9 +18,36 @@ def KSA(N,T,Key):
         #print(S)
     return S
 
+def KSA_STT(N,Key):
+    Key = binary_word(Key)
+    S = [i for i in range(0,N)]
+    licznik = 0
+    j = 0
+    count_of_marked_items = 0
+    count_of_items = len(S)
+    marked_item_list = [False]*count_of_items
+    threshhold = (count_of_items-1)/2
+    while(count_of_marked_items != count_of_items):
+        i = licznik % N
+        j =(j + S[i % N] + Key[licznik % len(Key)]) % N
+        S[i % N] , S[j % N] = S[j % N] , S[i % N]
+        
+        if count_of_marked_items < threshhold:
+            if marked_item_list[i] == False and marked_item_list[j] == False:
+                marked_item_list[i] == True
+                count_of_marked_items += 1
+        else:
+            if (marked_item_list[i] == False and marked_item_list[j] == True) or (i == j):
+                marked_item_list[i] == True
+                count_of_marked_items += 1
+        licznik += 1
+        marked_item_list[i], marked_item_list[j] = marked_item_list[j], marked_item_list[i] #swap
+    return S
+
 def KSA_RS(N,T,Key):
     S = [i for i in range(0,N)]
     L = len(Key)
+    #Key = binary_word(Key)
     for r in range(0,T):
         print(S)
         Top = []
